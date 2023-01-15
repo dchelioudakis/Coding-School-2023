8c8b684431a43c06c7e2c77530a0f80fee0a00e9
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-01-15 16:04:50 +0200

    Exercise #3

---
+++ Session-05/Session-05/PrimeNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    //3. Write a C# program that asks the user for an integer (n) and finds all the prime numbers from 1 to n.
+    public class PrimeNumbers
+    {
+        public int number { get; set; }
+        public string Calculate()
+        {
+            bool prime = false;
+            StringBuilder primeNumbers = new StringBuilder();
+
+            for (int i = 1; i <= number; i++)
+            {
+                prime = false;
+
+                if (i == 2)
+                {
+                    prime = true;
+                }
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                    else
+                    {
+                        prime = true;
+                    }
+                
+                }
+
+                if (prime)
+                {
+                    primeNumbers.Append(i);
+                    primeNumbers.Append(",");
+                }
+                
+
+                
+
+            }
+
+            return primeNumbers.ToString();
+        }
+    }
+}
--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -10,24 +10,22 @@ Console.WriteLine(myName.Reverse());
 
 
 // Exercise #2
+
+Console.WriteLine("Exercise #2");
 string userNumber;
-//Validate number
 do
 {
-    Console.WriteLine("Exercise #2");
     Console.WriteLine("Give an integer equal to or greater than 1:");
     userNumber = Console.ReadLine();
-} while (Convert.ToInt32(userNumber) < 1);
+} while (Convert.ToInt32(userNumber) < 1); //Validate number
 
 string option;
-//Validate option
 do
 {
-    Console.WriteLine("Exercise #2");
     Console.WriteLine(String.Format("For Sum of 1,...,{0} press 0", userNumber));
     Console.WriteLine(String.Format("For Product of 1,...,{0} press 1", userNumber));
     option = Console.ReadLine();
-} while (Convert.ToInt32(option) != 0 && Convert.ToInt32(option) != 1);
+} while (Convert.ToInt32(option) != 0 && Convert.ToInt32(option) != 1); //Validate option
 
 
 SumOrProduct givenSumOrProduct = new SumOrProduct();
@@ -41,5 +39,19 @@ else
     Console.WriteLine(String.Format("Product of 1,...,{0} is {1}", userNumber, givenSumOrProduct.Product()));
 }
 
+
+// Exercise #3
+
+Console.WriteLine("Exercise #3");
+
+do
+{
+    Console.WriteLine("Give an integer equal to or greater than 1:");
+    userNumber = Console.ReadLine();
+} while (Convert.ToInt32(userNumber) < 1); //Validate number
+
+PrimeNumbers userPrimeNumbers = new PrimeNumbers();
+userPrimeNumbers.number = Convert.ToInt32(userNumber);
+Console.WriteLine(userPrimeNumbers.Calculate());
 
 Console.ReadLine();
