4e4e0682d9d5de48073f54c88bda87a48ead6ac4
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-01-15 13:47:56 +0200

    Exercise #!

---
+++ Session-05/Session-05/NameReverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{   //1. Write a C# program that reverses a given string (your name).
+    public class NameReverse
+    {
+        public string Reverse()
+        {
+            string name = "Dimitris";
+            string reversedName = "";
+            StringBuilder mySb = new StringBuilder();
+
+            for (int i = name.Length-1; i >= 0; i--)
+            {
+                mySb.Append(name[i]);
+            }
+
+            reversedName = mySb.ToString();
+            
+            return reversedName;
+        }
+    }
+}
---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Session_05;
+
+
+// Exercise #1
+
+Console.WriteLine("Exercise #1");
+NameReverse myName = new NameReverse();
+Console.WriteLine(myName.Reverse());
+
+Console.ReadLine();
