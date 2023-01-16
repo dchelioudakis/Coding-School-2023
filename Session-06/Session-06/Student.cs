34107e2000869ad318cc6876b7f2de746132b943
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-01-16 15:29:39 +0200

    Student

---
+++ Session-06/Session-06/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06 {
+    public class Student : Person {
+        
+        public int RegistrationNumber { get; set; }
+        public Course[] Courses { get; set; }
+
+        public Student() {
+        }
+        public Student(int registrationNumber) {
+            RegistrationNumber = registrationNumber;
+        }
+        public Student(int registrationNumber, Course[] courses) {
+            RegistrationNumber = registrationNumber;
+            Courses = courses;
+        }
+
+        public void Attend(Course course, DateTime datetime) {
+        }
+        public void WriteExam(Course course, DateTime datetime) {
+
+        }
+    }
+}
