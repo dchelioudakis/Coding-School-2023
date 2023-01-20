b1fcd0352f9a2c8556f6e130df49cb5e4dbe773c
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-01-20 16:10:59 +0200

    Populate Courses, Students

--- Session-10/Session-10/Form1.Designer.cs
+++ Session-10/Session-10/Form1.Designer.cs
@@ -23,12 +23,35 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.grvStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grvStudents
+            // 
+            this.grvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvStudents.Location = new System.Drawing.Point(114, 75);
+            this.grvStudents.Name = "grvStudents";
+            this.grvStudents.RowHeadersWidth = 51;
+            this.grvStudents.RowTemplate.Height = 29;
+            this.grvStudents.Size = new System.Drawing.Size(356, 188);
+            this.grvStudents.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1318, 650);
+            this.Controls.Add(this.grvStudents);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grvStudents)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DataGridView grvStudents;
     }
 }
--- Session-10/Session-10/Form1.cs
+++ Session-10/Session-10/Form1.cs
@@ -1,7 +1,65 @@
+using UniLib;
+
 namespace Session_10 {
     public partial class Form1 : Form {
+
+        private BindingSource BsCourses = new BindingSource();
+        private BindingSource BsStudents = new BindingSource();
+        private BindingSource BsGrades = new BindingSource();
+        private BindingSource BsSheduledCourses = new BindingSource();
         public Form1() {
             InitializeComponent();
         }
+
+        private List<Course> GetCourses() {
+
+            List<Course> courses = new List<Course>();
+
+            Course courseOne = new Course() {
+                Code = "CRS1",
+                Subject = "Algebra",
+            };
+            courses.Add(courseOne);
+
+            Course courseTwo = new Course() {
+                Code = "CRS2",
+                Subject = "Physics",
+            };
+            courses.Add(courseTwo);
+
+
+            BsCourses.DataSource = courses;
+
+            return courses;
+        }
+        private void PopulateStudents() {
+
+            List<Course> courses = GetCourses();
+
+            List<Student> students = new List<Student>();
+
+            Student studentOne = new Student() {
+                Name = "Dimitris",
+                Age = 28,
+                RegistrationNumber = 1563,
+                Courses = { courses[0] },
+            };
+            students.Add(studentOne);
+
+            Student studentTwo = new Student() {
+                Name = "Stelios",
+                Age = 27,
+                RegistrationNumber = 1897,
+                Courses = { courses[0], courses[1] },
+            };
+            students.Add(studentTwo);
+
+
+            BsStudents.DataSource = students;
+        }
+
+        
+
+
     }
 }
