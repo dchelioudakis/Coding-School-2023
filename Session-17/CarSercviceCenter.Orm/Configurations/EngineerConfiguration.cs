87934571dd7db64fca22caf1d33f5b2b0e8ca788
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-01-31 15:30:37 +0200

    EngineerCongiguration

---
+++ Session-17/CarSercviceCenter.Orm/Configurations/EngineerConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using LibCarService;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSercviceCenter.Orm.Configurations {
+    public class EngineerConfiguration : IEntityTypeConfiguration<Engineer> {
+
+        public void Configure(EntityTypeBuilder<Engineer> builder) {
+            builder.ToTable("Engineer");
+
+            builder.HasKey(engineer => engineer.Id);
+
+            builder.Property(engineer => engineer.Id).ValueGeneratedOnAdd();
+            builder.Property(engineer => engineer.Name).HasMaxLength(50).IsRequired(true);
+            builder.Property(engineer => engineer.Surname).HasMaxLength(50).IsRequired(true);
+            builder.Property(engineer => engineer.SalaryPerMonth).HasMaxLength(50).IsRequired(true);
+
+            builder.HasOne(engineer => engineer.User)
+                 .WithMany(user => user.Engineers)
+                 .HasForeignKey(engineer => engineer.UserId);
+
+            builder.HasOne(engineer => engineer.Manager)
+                 .WithMany(manager => manager.Engineers)
+                 .HasForeignKey(engineer => engineer.ManagerId);
+        }
+
+
+    }
+}
