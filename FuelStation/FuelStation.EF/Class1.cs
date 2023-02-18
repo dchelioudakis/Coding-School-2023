7672e510ed96155c07a20d324e1450e9d84080b9
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-02-18 14:20:32 +0200

    Project Init

---
+++ FuelStation/FuelStation.EF/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace FuelStation.EF {
+    public class Class1 {
+
+    }
+}
---
+++ FuelStation/FuelStationDesktop/Form1.cs
@@ -0,0 +1,7 @@
+namespace FuelStationDesktop {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
