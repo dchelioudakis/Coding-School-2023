15ba5c82bf462cae6e792103a889ab8410e7a0d5
Dimitris Chelioudakis <dimchelioudakis@gmail.com>
2023-02-19 21:38:41 +0200

    Transaction Controller

---
+++ FuelStation/FuelStationAPI/DTO/Transaction/TransactionEditDto.cs
@@ -0,0 +1,18 @@
+﻿using FuelStation.Model.Enums;
+using FuelStation.Model;
+using FuelStationAPI.DTO.TransactionLine;
+
+namespace FuelStationAPI.DTO.Transaction {
+    public class TransactionEditDto {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public decimal TotalValue { get; set; }
+
+        //Relations
+        public int EmployeeId { get; set; }
+        public int CustomerId { get; set; }
+        
+        public List<TransactionLineEditDto> TransactionLines { get; set; }
+    }
+}
